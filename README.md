6bde8cdcedc105dfdede9b16369cbea2cafa9669
PRIMATEC\douaa.chtioui <douaa.chtioui@technica-engineering.de>
2021-11-06 15:55:44 +0100

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# sticky-notes
+A note taking board.
+
+to work with this app:
+clone it
+npm install 
+npm install express socket.io 
+node server.js
+then go to localhost:3010
+
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+	socket.broadcast.emit('test', 'testing if work');
+	socket.on('drawClick', (data) =>{
+		console.log(data);
+		//io.emit('draw', {event : data.event,lX : data.x, lY : data.y, cX : data.currentX, cY : data.currentY, color: data.color});
+		io.emit('draw', data);
+		//socket.emit('draw', data);
+	});
+
+	socket.on('addNote', (note) =>{
+		console.log(note);
+		socket.broadcast.emit('noteAdd', {innerHTML : note.innerHTML, id : note.id});
+		console.log('emited');
+	});
+	
+	
+});
+
+
+const PORT = 3010 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
